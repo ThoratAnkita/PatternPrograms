331b2e579bf2ab03e87fd2db05fd0c46476979bc
Ankita <ankitathorat68@gmail.com>
2025-02-28 00:05:26 +0530

    New pattern added!

---
+++ PatternPrograms/src/patterns/Pattern_28.java
@@ -0,0 +1,38 @@
+package patterns;
+
+public class Pattern_28 {
+
+	public static void main(String[] args) {
+		char ch = 'A';
+        int rows = 5;
+
+        for (int i=1;i<=rows;i++) {
+            for (int j=i;j<rows;j++) {
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++) {
+                if (ch>'Z') break;
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+
+        for (int i=rows-1;i>=1;i--) {
+            for (int j=rows-i;j>0;j--) {
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++) {
+                if (ch>'Z') break;
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+        if (ch=='Z') {
+            System.out.println(" ".repeat(rows-1) + ch);
+        }
+
+	}
+
+}
